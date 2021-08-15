c3909dca82bc722b9f6400578f4cef20ea82e468
Wesley <wesley.duff@charter.com>
2021-08-15 15:52:18 -0600

    (working-state):basics

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Make one query for all api endpoints from these lower microservices.
 - mobile
 - transit
 
+#Server : 
+This is what we wil be using
+https://www.apollographql.com/docs/apollo-server/
+
 #iOS
 https://www.apollographql.com/docs/ios/
 
@@ -14,3 +18,9 @@ https://www.apollographql.com/docs/android/
 
 #React
 https://www.apollographql.com/docs/react/
+
+## RUN THE APP
+Running the app requires you to run an a node script
+- `npm start`
+
+Visit http://localhost:3000/ and click on the button to query your service.
---
+++ index.js
@@ -0,0 +1,45 @@
+const
+    { ApolloServer, gql } = require('apollo-server'),
+    sessions = require('./data/sessions.json')
+;
+
+const typeDefs = gql`    
+    type Query{
+        sessions:[Session]
+    }
+    
+    type Session {
+        id: ID!
+        title: String!
+        description: String
+        startsAt: String
+        endsAt: String
+        room: String
+        day: String
+        format: String
+        track: String
+        @deprecated(
+            reason: "Too many sessions do not fit into a single track, we will be migrating to a tags based system in the future..."
+        )
+        level: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        sessions: () => {
+            return sessions
+        }
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+
+server
+    .listen({
+        port: process.env.PORT || 3000
+    })
+    .then(({url}) => {
+        console.log(`INFO : SERVER NOTIFICATION : graph QL running at ${url}`)
+    });
